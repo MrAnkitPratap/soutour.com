4adb6c552e927df1bd5547c9d0d29fb874b2e62b
ANKIT PRATAP <111798371+thelogodesigner1@users.noreply.github.com>
2024-04-28 10:19:27 +0530

    Update Card.jsx

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Card = ({ amount, img, checkoutHandler }) => {
     return (
         <Stack>
-            <Button onClick={() => checkoutHandler(amount)} sx={{color:'white', height:'230px' ,backgroundImage:'radial-gradient( circle 325px at 19.2% 64.8%,  rgba(254,62,101,1) 9.7%, rgba(166,24,146,1) 91.3% )', borderRadius:'10px'}}>Book Now</Button>
+            <Button onClick={() => checkoutHandler(amount)} sx={{color:'white', height:'30px' ,backgroundImage:'radial-gradient( circle 325px at 19.2% 64.8%,  rgba(254,62,101,1) 9.7%, rgba(166,24,146,1) 91.3% )', borderRadius:'10px'}}>Book Now</Button>
         </Stack>
     )
 }
